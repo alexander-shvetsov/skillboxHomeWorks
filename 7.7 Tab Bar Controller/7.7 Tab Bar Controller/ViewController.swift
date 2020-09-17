a311726f421405deeeeadc4cf3c9229e920f4d8a
Alexander Shvetsov <alex85.ekb@gmail.com>
2020-09-17 09:22:38 +0500

    added blank project
    added project 7.7 tab bar controller

---
+++ 7.7 Tab Bar Controller/7.7 Tab Bar Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7.7 Tab Bar Controller
+//
+//  Created by Shvetsov_AV on 17.09.2020.
+//  Copyright © 2020 Alexander Shvetsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
